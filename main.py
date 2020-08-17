41159b4723ba74321e0be431289f254c094a38f3
Yash Chellani <35882565+yashchellani@users.noreply.github.com>
2020-08-17 15:32:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
+from flask import Flask, request
+import os
+
+server = Flask(__name__)
 
 
 def get_url():
@@ -24,13 +28,31 @@ def main():
 
 
 def get_image_url():
-    allowed_extension = ['jpg', 'jpeg', 'png', 'JPG', "JPEG", "PNG"]
+    allowed_extension = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG']
     file_extension = ''
     while file_extension not in allowed_extension:
         url = get_image_url()
         file_extension = re.search("([^.]*)$", url).group(1).lower()
     return url
 
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://your_heroku_project.com/" +
+                    "1190888035:AAGeJ9316R95NqJLFefV5vQA-UL4np11V2c")
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 if __name__ == '__main__':
     main()
